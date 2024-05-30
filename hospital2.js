535a0289d01cc517ba06ba0873b715a9c6ac3a26
adityapra1 <164087454+adityapra1@users.noreply.github.com>
2024-05-30 11:51:16 +0530

    Add files via upload

---
+++ hospital2.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active'); 
+}
+
+var swiper = new Swiper(".home", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
